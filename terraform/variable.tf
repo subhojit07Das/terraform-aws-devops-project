b1e0502f77c427de15fe23d49ec82c1f4b35bc06
Subhojit Das <subhojit05das@gmail.com>
2026-01-18 20:12:05 +0530

    Added Terraform AWS provider and base security group

---
+++ terraform/variable.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+	description = "EC2 instance type"
+	type = string
+	default = "t3.micro"
+}
+
+variable "key_name" {
+	description = "SSH key pair name"
+	type = string
+}
+
+variable "project_name" {
+  description = "Project tag name"
+  type        = string
+  default     = "terraform-devops-project"
+}
